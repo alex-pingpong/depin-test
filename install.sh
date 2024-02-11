e42afac6f4257ab3d3c6132b2b375e01a9fb2f5b
Alex <alex@toriai.com>
2024-02-11 20:31:57 +0800

    feat: add file status check on shell script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-sudo apt update && sudo apt install git curl build-essential liblzma-dev libbz2-dev libffi-dev libncursesw5-dev zlib1g-dev libreadline-dev libsqlite3-dev libssl-dev
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+git pull
+sudo apt update && sudo apt install -y git curl build-essential liblzma-dev libbz2-dev libffi-dev libncursesw5-dev zlib1g-dev libreadline-dev libsqlite3-dev libssl-dev
+[ -f "$HOME/.asdf/asdf.sh" ] || git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
 
 . "$HOME/.asdf/asdf.sh"
 asdf plugin-add python
@@ -10,3 +11,5 @@ asdf global python 3.12.2
 asdf plugin-add poetry
 asdf install poetry 1.7.1
 asdf global poetry 1.7.1
+
+poetry install
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+git pull
+[ -f "$HOME/.asdf/asdf.sh" ] || bash install.sh
 . "$HOME/.asdf/asdf.sh"
 poetry install
 poetry run locust $@
